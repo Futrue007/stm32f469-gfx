ad5d7a239537685d31af897621915448713dbffd
maweifu <maweifu@huami.com>
2019-05-30 10:09:17 +0800

    init version

---
+++ build/MINGW32_NT-6.2/generated/images/src/stm32f469_logo.d
@@ -0,0 +1,5 @@
+build/MINGW32_NT-6.2/generated/images/src/stm32f469_logo.o: \
+ generated/images/src/stm32f469_logo.cpp \
+ touchgfx//framework/include/touchgfx/hal/Config.hpp
+
+touchgfx//framework/include/touchgfx/hal/Config.hpp:
